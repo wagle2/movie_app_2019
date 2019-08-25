a419fce90b51ceedc26dc9cf3a5106c0c2f819f7
wagle2 <wagle2@github.com>
2019-08-25 13:46:33 +0900

    Updates

---
+++ precache-manifest.9eca32e85bde6d25fc5d8024fa69a155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac90e331a996240370aedd4e1a4bd63",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "af781289eff767c9a2c0",
+    "url": "/movie_app_2019/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "d09f1130db4e24382e44",
+    "url": "/movie_app_2019/static/js/2.4243bc04.chunk.js"
+  },
+  {
+    "revision": "af781289eff767c9a2c0",
+    "url": "/movie_app_2019/static/js/main.8fe3307f.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
